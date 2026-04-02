e04e435b52670039fb2d5237841c51a426605a30
rei-pinehills <a.reiko0117@gmail.com>
2026-04-02 12:31:37 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+function lookup() {
+    const domain = document.getElementById("domain").value;
+    const url = `https;//dns.google/resolve?name=${domain}&type=A`;
+
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            const result = document.getElementById("result");
+            if (data.Answer) {
+                let html = `<h3>Results for: ${domain}</h3>`;
+                data.Answer.forEach(record => {
+                    html += `<p>${record.name} -> ${record.data}</p>`;
+                });
+                result.innerHTML = html;
+            }else {
+                result.innerHTML = "<p>No results found.</p>";
+            }
+
+            });
+        }
+
